496212982a293ef85c30eff6cda7034e2e3be91f
b-delacruz <brianmdelacruz@gmail.com>
2022-06-06 22:59:33 -0400

    add cached elements

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const aboutBtn = document.querySelector("#about-me")
+const nftBtn = document.querySelector("#nft-collection")
+const workBtn = document.querySelector("#my-work")
+const contact = document.querySelector("#contact")
+
+console.log(aboutBtn)
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
+
