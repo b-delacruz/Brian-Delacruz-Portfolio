b9ab69e66903007d1e2e6ac5e2fe35f9cdd34ac5
b-delacruz <brianmdelacruz@gmail.com>
2022-06-07 19:41:28 -0400

    toggle favicon easet egg button

--- js/app.js
+++ js/app.js
@@ -10,10 +10,27 @@ const aboutBtn = document.querySelector("#about-me")
 const nftBtn = document.querySelector("#nft-collection")
 const workBtn = document.querySelector("#my-work")
 const contact = document.querySelector("#contact")
+const homeBtn = document.querySelector("#home-page")
 
-console.log(aboutBtn)
+const favicon = document.querySelector("#favicon")
+const bugBtn = document.getElementById("bug")
+console.log(favicon)
 /*----------------------------- Event Listeners -----------------------------*/
-
+bugBtn.addEventListener("click", handleEvent)
 
 /*-------------------------------- Functions --------------------------------*/
 
+let bugFavicon = false
+
+function handleEvent(){
+  bugFavicon = !bugFavicon
+  changeFavicon()
+  console.log(bugFavicon)
+}
+
+
+function changeFavicon() {
+bugFavicon
+  ? favicon.setAttribute('href', "/assets/images/bug.jpg") : favicon.setAttribute('href', "/assets/images/Brian.jpg")
+  console.log(bugFavicon)
+}
