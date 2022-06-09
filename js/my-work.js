1875c27ca691ba0fa6daef4910280fee935960d5
b-delacruz <brianmdelacruz@gmail.com>
2022-06-09 14:28:42 -0400

    update nft collection

--- js/my-work.js
+++ js/my-work.js
@@ -34,20 +34,6 @@ let myWork = [
     github:'https://github.com/b-delacruz/ttt-weekend',
     description:'A tic tac toe game',
   },
-  {
-    title:'Tic Tac Toe',
-    deployment:'https://ttt-delacruz-games.netlify.app/',
-    image:'/assets/mywork/ttt.png',
-    github:'https://github.com/b-delacruz/ttt-weekend',
-    description:'A tic tac toe game',
-  },
-  {
-    title:'Tic Tac Toe',
-    deployment:'https://ttt-delacruz-games.netlify.app/',
-    image:'/assets/mywork/ttt.png',
-    github:'https://github.com/b-delacruz/ttt-weekend',
-    description:'A tic tac toe game',
-  }
 ]
 
 export{
