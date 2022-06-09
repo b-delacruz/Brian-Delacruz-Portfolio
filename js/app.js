5c5d6c3af6be9f2c22bed221c528eb2d1c1416f5
b-delacruz <brianmdelacruz@gmail.com>
2022-06-09 13:59:08 -0400

    add projects to my work page

--- js/app.js
+++ js/app.js
@@ -1,19 +1,36 @@
 /*-------------------------------- Constants --------------------------------*/
 import { myWork } from "./my-work.js"
 
-console.log(myWork)
+let project = {
+    title:'Tic Tac Toe',
+    deployment:'https://ttt-delacruz-games.netlify.app/',
+    image:'/assets/mywork/ttt.png',
+    github:'https://github.com/b-delacruz/ttt-weekend',
+    description:'A tic tac toe game',
+}
 
+let myProject = myWork.map(project =>
+`
+<div class="card" style="width: 18rem;">
+  <img src="${project.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${project.title}</h5>
+    <p class="card-text">${project.description}</p>
+    <a href="${project.github}" class="git-btn">GitHub</a>
+    <a href="${project.deployment}" class="deploy-btn">Deploy</a>
+  </div>
+</div>
+`
+).join('')
+
+
+console.log(myProject)
 /*-------------------------------- Variables --------------------------------*/
 
 
 /*------------------------ Cached Element References ------------------------*/
 const cardContainer = document.getElementById('card-container')
-
-const aboutBtn = document.querySelector("#about-me")
-const nftBtn = document.querySelector("#nft-collection")
-const workBtn = document.querySelector("#my-work")
-const contact = document.querySelector("#contact")
-const homeBtn = document.querySelector("#home-page")
+cardContainer.innerHTML = myProject
 
 const favicon = document.querySelector("#favicon")
 const bugBtn = document.getElementById("bug")
--- js/my-work.js
+++ js/my-work.js
@@ -26,6 +26,27 @@ let myWork = [
     image:'/assets/mywork/ttt.png',
     github:'https://github.com/b-delacruz/ttt-weekend',
     description:'A tic tac toe game',
+  },
+  {
+    title:'Tic Tac Toe',
+    deployment:'https://ttt-delacruz-games.netlify.app/',
+    image:'/assets/mywork/ttt.png',
+    github:'https://github.com/b-delacruz/ttt-weekend',
+    description:'A tic tac toe game',
+  },
+  {
+    title:'Tic Tac Toe',
+    deployment:'https://ttt-delacruz-games.netlify.app/',
+    image:'/assets/mywork/ttt.png',
+    github:'https://github.com/b-delacruz/ttt-weekend',
+    description:'A tic tac toe game',
+  },
+  {
+    title:'Tic Tac Toe',
+    deployment:'https://ttt-delacruz-games.netlify.app/',
+    image:'/assets/mywork/ttt.png',
+    github:'https://github.com/b-delacruz/ttt-weekend',
+    description:'A tic tac toe game',
   }
 ]
 
