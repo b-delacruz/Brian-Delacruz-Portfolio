fc6640ae8ce4c0edafecf0cee051311feea19407
b-delacruz <brianmdelacruz@gmail.com>
2022-06-10 11:37:34 -0400

    add functional light dark button to each page

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
 
 const favicon = document.querySelector("#favicon")
 const bugBtn = document.getElementById("bug")
-const lightDarkBtn = document.querySelector("#light-dark-button")
-const body = document.querySelector("body")
+
 
 bugBtn.addEventListener("click", handleEvent)
-lightDarkBtn.addEventListener("click", toggleLightDark)
-
 
 
 let bugFavicon = false
-
-checkDarkPref()
 
 function handleEvent(){
   bugFavicon = !bugFavicon
@@ -24,17 +19,4 @@ function changeFavicon() {
 bugFavicon
   ? favicon.setAttribute('href', "/assets/images/bug.jpg") : favicon.setAttribute('href', "/assets/images/fix.jpg")
   console.log(bugFavicon)
-}
-
-function toggleLightDark() {
-  body.className = body.className === "dark" ? "" : "dark"
-}
-
-function checkDarkPref() {
-  if (
-    window.matchMedia("(prefers-color-scheme:dark)").matches &&
-    body.className !== "dark"
-  ) {
-    toggleLightDark()
-  }
 }
