f9aa6319dc7bd1f00a7f31b0d1605fe33898b9f9
b-delacruz <brianmdelacruz@gmail.com>
2022-06-09 21:40:05 -0400

    add light dark mode button

--- js/app.js
+++ js/app.js
@@ -1,27 +1,17 @@
-/*-------------------------------- Constants --------------------------------*/
-
-
-
-
-/*-------------------------------- Variables --------------------------------*/
-
-
-
-/*------------------------ Cached Element References ------------------------*/
-
 
 const favicon = document.querySelector("#favicon")
 const bugBtn = document.getElementById("bug")
+const lightDarkBtn = document.querySelector("#light-dark-button")
+const body = document.querySelector("body")
 
-
-// /*----------------------------- Event Listeners -----------------------------*/
 bugBtn.addEventListener("click", handleEvent)
+lightDarkBtn.addEventListener("click", toggleLightDark)
 
-console.log(bugBtn)
 
-// /*-------------------------------- Functions --------------------------------*/
 
 let bugFavicon = false
+
+checkDarkPref()
 
 function handleEvent(){
   bugFavicon = !bugFavicon
@@ -34,4 +24,17 @@ function changeFavicon() {
 bugFavicon
   ? favicon.setAttribute('href', "/assets/images/bug.jpg") : favicon.setAttribute('href', "/assets/images/fix.jpg")
   console.log(bugFavicon)
+}
+
+function toggleLightDark() {
+  body.className = body.className === "dark" ? "" : "dark"
+}
+
+function checkDarkPref() {
+  if (
+    window.matchMedia("(prefers-color-scheme:dark)").matches &&
+    body.className !== "dark"
+  ) {
+    toggleLightDark()
+  }
 }
--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,29 @@
 
-const nameOf = document.getElementById('name')
+
+const submitBtn = document.querySelector('#submit')
+const nameInput = document.querySelector('#name')
+const numberInput = document.querySelector('#number')
+
+console.log(submitBtn)
+
+init()
+
+submitBtn.addEventListener('submit',function(evt){
+  evt.preventDefault()
+  submitBtn.submit();
+
+})
 
 
-nameOf.textContent = "Hello"
+function getData (){
+  let newName = nameInput.textContent
+  nameInput.innerHTML = newName
+
+  console.log(newName)
+}
+
+function init(){
+  nameInput.textContent = 'hello'
+}
+
+console.log(nameInput.textContent)
